6ff11c3773ac1cab4cc4267b14276b5a68677273
VzGriffin <114660162+VzGriffin@users.noreply.github.com>
2023-02-02 13:47:32 +0000

    test

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -10,5 +10,30 @@ namespace ConsoleAppProject.App01
     /// </author>
     public class DistanceConverter
     {
+
+        public DistanceConverter()
+        {
+
+        }
+
+        public void Run()
+        {
+            //step 1 Input to me
+            Console.WriteLine("Please enter the number of Miles");
+            string input = Console.ReadLine();
+            int miles = Convert.ToInt32(input);
+            Console.WriteLine("Miles entered is: " + miles);
+
+
+            //step 2 - Converting Miles to feet
+            const int MILES_TO_FEET = 5280;
+            int feet = miles * MILES_TO_FEET;
+
+            //step 3 Output Converted to feet
+
+            Console.WriteLine(miles + " miles is " + feet + " feet ");
+
+
+        }
     }
 }
